132230613c445c0a73491724e537a22a48e7a8aa
Alex J. Luna <alexjose.lunamendoza@nttdata.com>
2023-04-30 10:39:36 +0200

    Services definition

--- web/modules/custom/hello_world_luna/src/HelloWorldLunaSalutation.php
+++ web/modules/custom/hello_world_luna/src/HelloWorldLunaSalutation.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\hello_world_luna;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
@@ -12,9 +13,30 @@ class HelloWorldLunaSalutation {
   use StringTranslationTrait;
 
   /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * HelloWorldLunaSalutation
+   * @param \Drupal\Core\Config\ConfigFactoryInterface
+   *  $config_factory
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * Return the salutation
    */
   public function getSalutation() {
+
+    $config = $this->configFactory->get('hello_world_luna.custom_salutation');
+    $salutation = $config->get('salutation');
+    if($salutation !== '' && $salutation) {
+      return $salutation;
+    }
+
     $time = new \DateTime();
     if((int) $time->format('G') >= 00 && (int) $time->format('G') < 12) {
       return $this->t('Good morning world');
